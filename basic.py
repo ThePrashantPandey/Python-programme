4e94aee0d6369d31e5e6f84d382e4a7e6008c8f5
Prashant91443 <prashant.91443@mygyanvihar.com>
2024-02-02 10:50:22 +0530

    uncomment the code

--- basic.py
+++ basic.py
@@ -1,7 +1,7 @@
 #solution -1 (Using exponentiotion)
-# a = eval(input("enter a number :-"))
-# sr = a**(1/2)  # square root of the number
-# print(sr)
+a = eval(input("enter a number :-"))
+sr = a**(1/2)  # square root of the number
+print(sr)
 
 
 #solution -2 (Using math module)
