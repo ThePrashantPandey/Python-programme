5fb0e9b2153028733b99af32ef52756bdb08ecb6
Prashant91443 <prashant.91443@mygyanvihar.com>
2024-02-02 10:05:43 +0530

    Using exponentiotion , Using math module

---
+++ basic.py
@@ -0,0 +1,12 @@
+#solution -1 (Using exponentiotion)
+# a = eval(input("enter a number :-"))
+# sr = a**(1/2)  # square root of the number
+# print(sr)
+
+
+#solution -2 (Using math module)
+
+import math
+a = eval(input("enter a number :-"))
+sr = math.sqrt(a)  # square root of the number
+print(sr)
